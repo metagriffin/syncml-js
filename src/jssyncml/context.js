a691a17ae21f97295aabf1d671c383b64f6f5162
metagriffin <mg.github@uberdev.org>
2012-11-10 16:21:41 -0500

    added all-in-one adapter configuration method

--- src/jssyncml/context.js
+++ src/jssyncml/context.js
@@ -19,9 +19,9 @@ var idxdb = ( typeof(window) != 'undefined' && window.indexedDB ) || this.indexe
 define([
   'underscore',
   'elementtree',
+  './logging',
   './common',
   './constant',
-  './codec',
   './storage',
   './router',
   './synchronizer',
@@ -30,9 +30,9 @@ define([
 ], function(
   _,
   ET,
+  logging,
   common,
   constant,
-  codec,
   storage,
   router,
   synchronizer,
@@ -40,6 +40,7 @@ define([
   adapter
 ) {
 
+  var log = logging.getLogger('jssyncml.context');
   var exports = {};
 
   //---------------------------------------------------------------------------
@@ -51,15 +52,15 @@ define([
       this.storage      = options.storage || idxdb;
       this.dbname       = ( options.prefix || '' ) + 'jssynmcl';
       this.autoCommit   = options.autoCommit == undefined ? true : options.autoCommit;
-      this.router       = options.router || new router.Router();
+      this.router       = options.router || new router.SmartRouter();
       this.synchronizer = options.synchronizer || new synchronizer.Synchronizer();
       this.protocol     = options.protocol || new protocol.Protocol();
-      this.codec        = options.codec || codec.Codec.factory(constant.CODEC_XML);
+      this.codec        = options.codec || constant.CODEC_XML;
       this._db          = null;
     },
 
     //-------------------------------------------------------------------------
-    getAdapter: function(options, cb) {
+    getAdapter: function(options, devInfo, cb) {
       options = options || {};
       var self = this;
       if ( this._db == undefined )
@@ -69,17 +70,105 @@ define([
             return cb(err);
           self._db = db;
           self._db.onerror = function(event) {
-            console.log('ERROR: jssyncml.context.db request failed with: '
-                        + event.target.errorCode);
+            // todo: remove this?...
+            log.error('jssyncml.context.db request failed with: '
+                      + event.target.error);
           };
-          self.getAdapter(options, cb);
+          self.getAdapter(options, devInfo, cb);
         });
       }
       else
       {
-        var ret = new adapter.Adapter(this, options);
+        var ret = new adapter.Adapter(this, options, devInfo);
         return ret._load(cb);
       }
+    },
+
+    //-------------------------------------------------------------------------
+    getEasyClientAdapter: function(options, cb) {
+      // options should be:= {
+      //   // devID,
+      //   // name,
+      //   devInfo: {},
+      //   stores: [],
+      //   peer: {},
+      //   routes: [
+      //     [ source, target ],
+      //   ]
+      // }
+      // response: cb(err, adapter, stores, peer);
+
+      var self = this;
+
+      var ret = {
+        adapter: null,
+        stores: [],
+        peer: null
+      };
+
+      var setupAdapter = function(cb) {
+        var adapterOptions = _.omit(options, 'devInfo', 'stores', 'peers', 'routes');
+        self.getAdapter(adapterOptions, options.devInfo, function(err, adapter) {
+          if ( err )
+            return cb(err);
+          ret.adapter = adapter;
+          if ( adapter.devInfo )
+            return cb();
+          adapter.setDevInfo(options.devInfo, cb);
+        });
+      };
+
+      var setupStores = function(cb) {
+        common.cascade(options.stores, function(storeInfo, cb) {
+          var store = ret.adapter.getStore(storeInfo.uri);
+          if ( store != undefined )
+          {
+            if ( storeInfo.agent )
+              store.agent = storeInfo.agent;
+            ret.stores.push(store);
+            return cb();
+          }
+          ret.adapter.addStore(storeInfo, function(err, store) {
+            if ( err )
+              return cb(err);
+            ret.stores.push(store);
+            return cb();
+          });
+        }, cb);
+      };
+
+      var setupPeer = function(cb) {
+        var peer = _.find(ret.adapter.getPeers(), function(p) {
+          return p.url == options.peer.url;
+        });
+        if ( peer )
+        {
+          ret.peer = peer;
+          return cb();
+        }
+        ret.adapter.addPeer(options.peer, function(err, peer) {
+          if ( err )
+            return cb(err);
+          ret.peer = peer;
+          common.cascade(options.routes, function(route, cb) {
+            ret.peer.addRoute(route[0], route[1], cb);
+          }, cb);
+        });
+      };
+
+      setupAdapter(function(err) {
+        if ( err )
+          return cb(err);
+        setupStores(function(err) {
+          if ( err )
+            return cb(err);
+          setupPeer(function(err) {
+            if ( err )
+              return cb(err);
+            cb(null, ret.adapter, ret.stores, ret.peer);
+          });
+        });
+      });
     },
 
   });
